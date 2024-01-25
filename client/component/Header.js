719b9eb8f7d8ccfbaa1d6109358ac0c1dd2cc01d
unknown <sakarim9124@gmail.com>
2024-01-25 21:58:33 +0530

    updated

---
+++ client/component/Header.js
@@ -0,0 +1,59 @@
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Avatar } from "react-native-paper";
+import { colors } from "../styles/common";
+import { useNavigation, useRoute } from "@react-navigation/native";
+
+const Header = ({ back, emtpyCart = false }) => {
+  //misc
+  const route = useRoute();
+  const navigate = useNavigation();
+  //func
+  const handeEmptyCart = () => {
+    console.log("Ëmpty Cart");
+  };
+
+  return (
+    <>
+      {back && (
+        <TouchableOpacity
+          style={{
+            position: "absolute",
+            left: 20,
+            top: 40,
+            zIndex: 10,
+          }}
+          onPress={navigate.goBack()}
+        >
+          <Avatar.Icon
+            style={{ backgroundColor: colors.color4 }}
+            icon={"arrow-left"}
+            color={
+              route.name === "productdetails" ? colors.color2 : colors.color3
+            }
+          />
+        </TouchableOpacity>
+      )}
+      <TouchableOpacity
+        style={{
+          position: "absolute",
+          right: 20,
+          top: 40,
+          zIndex: 10,
+        }}
+        onPress={emtpyCart ? handeEmptyCart : navigate.navigate("cart")}
+      >
+        <Avatar.Icon
+          style={{ backgroundColor: colors.color4 }}
+          icon={emtpyCart ? "delete-outline" : "cart-outline"}
+          color={
+            route.name === "productdetails" ? colors.color2 : colors.color3
+          }
+        />
+      </TouchableOpacity>
+    </>
+  );
+};
+
+export default Header;
+
+const styles = StyleSheet.create({});
--- client/screens/Home.js
+++ client/screens/Home.js
@@ -1,20 +1,128 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
-import { defaultStyle } from "../styles/common";
+import {
+  Button,
+  ScrollView,
+  StyleSheet,
+  Text,
+  Touchable,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { colors, defaultStyle } from "../styles/common";
+import Header from "../component/Header";
+import { Avatar } from "react-native-paper";
+import { useState } from "react";
 
 const Home = () => {
+  //misc
+  const categories = [
+    { category: "One", id: 1 },
+    { category: "Two", id: 2 },
+    { category: "Three", id: 3 },
+    { category: "Four", id: 4 },
+    { category: "Five", id: 5 },
+    { category: "Six", id: 6 },
+    { category: "Seven", id: 7 },
+    { category: "Eight", id: 8 },
+  ];
+  //state
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [activeSearch, setActiveSearch] = useState("");
+
+  //func
+  const handleCategory = (val) => {
+    console.log(val);
+    setSelectedCategory(val);
+  };
+
   return (
     <View style={defaultStyle}>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
+      <Header back={false} />
+
+      {/* heading row */}
+      <View
+        style={{
+          paddingTop: 70,
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <View>
+          <Text style={{ fontSize: 25 }}>Our</Text>
+          <Text style={{ fontSize: 25, fontWeight: "900" }}> Products</Text>
+        </View>
+
+        {/* searchbar */}
+        <View>
+          <TouchableOpacity>
+            <Avatar.Icon
+              icon={"magnify"}
+              color="gray"
+              size={50}
+              style={{ backgroundColor: colors.color2, elevation: 12 }}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+      {/* categories */}
+      <View
+        style={{
+          flexDirection: "row",
+          height: 80,
+        }}
+      >
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{
+            alignItems: "center",
+          }}
+        >
+          {categories.map((item) => {
+            const { category, id } = item;
+            return (
+              <TouchableOpacity
+                key={id}
+                style={[
+                  styles.button,
+                  {
+                    backgroundColor:
+                      selectedCategory === id ? colors.color1 : colors.color5,
+                  },
+                ]}
+                onPress={() => handleCategory(id)}
+              >
+                <Text
+                  style={[
+                    styles.buttonText,
+                    { color: selectedCategory === id ? colors.color2 : "gray" },
+                  ]}
+                >
+                  {category}
+                </Text>
+              </TouchableOpacity>
+            );
+          })}
+        </ScrollView>
+      </View>
+
+      {/* products */}
     </View>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: colors.color5,
+    borderRadius: 100,
+    margin: 5,
+    alignItems: "center",
+    padding: 10, // Adjust as needed
+  },
+  buttonText: {
+    fontSize: 12,
+    color: "gray",
+  },
+});
