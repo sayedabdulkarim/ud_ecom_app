f2af7ae3361b18ccda44f104e8582773b1613c22
unknown <sakarim9124@gmail.com>
2024-01-27 23:45:26 +0530

    updated

--- client/component/ConfirmOrderItem.js
+++ client/component/ConfirmOrderItem.js
@@ -1,10 +1,43 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import React from "react";
 
-const ConfirmOrderItem = () => {
+const ConfirmOrderItem = ({ price, quantity, image, name }) => {
   return (
-    <View>
-      <Text>ConfirmOrderItem</Text>
+    <View
+      style={{
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        margin: 10,
+      }}
+    >
+      <Image
+        source={{
+          uri: image,
+        }}
+        style={{
+          width: 50,
+          height: 50,
+          resizeMode: "contain",
+        }}
+      />
+
+      <Text>{name}</Text>
+      <View
+        style={{
+          flexDirection: "row",
+        }}
+      >
+        <Text>{quantity}</Text>
+        <Text
+          style={{
+            marginHorizontal: 10,
+          }}
+        >
+          {"X"}
+        </Text>
+        <Text>₹ {price.toFixed(2)}</Text>
+      </View>
     </View>
   );
 };
