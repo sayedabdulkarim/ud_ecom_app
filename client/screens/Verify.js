727aeb2af355c574a5cff371e4ef75140cda3fbb
unknown <sakarim9124@gmail.com>
2024-01-28 18:13:58 +0530

    updated

--- client/screens/Verify.js
+++ client/screens/Verify.js
@@ -1,14 +1,83 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { useState } from "react";
+import {
+  colors,
+  defaultStyle,
+  formHeading,
+  inputOptions,
+  inputStyling,
+  formStyles as styles,
+} from "../styles/common";
+import { Button, TextInput } from "react-native-paper";
+import Footer from "../component/Footer";
 
-const Verify = () => {
+const Verify = ({ navigation }) => {
+  //misc
+  const loading = false;
+  //state
+  const [otp, setOtp] = useState("");
+  const [password, setPassword] = useState("");
+
+  //func
+  const handleSubmit = () => {
+    alert({
+      otp,
+      password,
+    });
+    navigation.navigate("login");
+  };
+
   return (
-    <View>
-      <Text>Verify</Text>
-    </View>
+    <>
+      <View style={{ ...defaultStyle, backgroundColor: colors.color2 }}>
+        {/* heading  */}
+
+        <View
+          style={{
+            marginBottom: 20,
+          }}
+        >
+          <Text style={formHeading}>Forgot Password</Text>
+        </View>
+
+        <View style={styles.container}>
+          <TextInput
+            {...inputOptions}
+            placeholder="OTP"
+            value={otp}
+            keyboardType="number-pad"
+            onChangeText={setOtp}
+          />
+          <TextInput
+            {...inputOptions}
+            placeholder="New Password"
+            value={password}
+            secureTextEntry
+            onChangeText={setPassword}
+          />
+          <Button
+            loading={loading}
+            style={styles.btn}
+            textColor={colors.color2}
+            disabled={otp === "" || password === ""}
+            onPress={() => handleSubmit()}
+          >
+            Reset
+          </Button>
+          <Text style={styles.or}>OR</Text>
+
+          <TouchableOpacity
+            activeOpacity={0.8}
+            onPress={() => navigation.navigate("forgotpassword")}
+          >
+            <Text style={styles.link}>Resend OTP</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      <Footer activeRoute="profile" />
+    </>
   );
 };
 
 export default Verify;
-
-const styles = StyleSheet.create({});
